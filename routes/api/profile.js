c2408bb8dd6569fcb0e307a4ba4bb86fc34f11ee
chidera <dexiouz@gmail.com>
2019-03-01 17:37:22 +0100

    added GET user by id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -55,6 +55,27 @@ router.get("/handle/:handle", (req, res) => {
     .catch(err => res.status(404).json(err));
 });
 
+// @route GET api/profile/user/:user_id
+// @desc Get profile by id
+// @access public
+router.get("/user/:user_id", (req, res) => {
+  const errors = {};
+
+  Profile.findOne({ user: req.params.user_id })
+    .populate("user", ["name", "avatar"])
+    .then(profile => {
+      if (!profile) {
+        errors.noproflie = "This person has no profile yet";
+        res.status(404).json(errors);
+      }
+
+      res.json(profile);
+    })
+    .catch(err =>
+      res.status(404).json({ profile: "This user has no profile" })
+    );
+});
+
 // @route POST api/profiles
 // @desc CREATE or edit user profile
 // @access private
