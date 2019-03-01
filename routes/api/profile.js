25ed31d7c733dd4280ada38dd1c52bf9f51bc422
chidera <dexiouz@gmail.com>
2019-03-01 13:10:50 +0100

    created current user  profile  route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,36 @@
 const express = require("express"),
-  router = express.Router();
+  mongoose = require("mongoose"),
+  passport = require("passport"),
+  router = express.Router(),
+  // Load profile model
+  Profile = require("../../models/Profile"),
+  // Load profile model
+  User = require("../../models/User");
 
 // @route GET api/profiles/test
 // @desc Test profiles route
 // @access public
 router.get("/test", (req, res) => res.json({ msg: "profiles works" }));
+
+// @route GET api/profiles
+// @desc Get current user profile
+// @access private
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const errors = {};
+
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        if (!profile) {
+          (errors.noproflie = "This user has no profile"),
+            res.status(404).json(errors);
+        }
+        res.json(profile);
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
 
 module.exports = router;
