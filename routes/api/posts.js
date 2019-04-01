738194a5f6e35357f2392c10a3ac9cc5567db127
chidera <dexiouz@gmail.com>
2019-04-01 19:16:51 +0100

    can now get a single post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -12,6 +12,25 @@ const express = require("express"),
 // @access public
 router.get("/test", (req, res) => res.json({ msg: "posts works" }));
 // @route GET api/posts
+// @desc Get posts
+// @access public
+router.get('/', (req, res) => {
+  Post.find()
+    .sort({ date: -1 })
+    .then(posts => res.json(posts))
+    .catch(err => res.status(404));
+})
+
+// @route GET api/posts/:id
+// @desc Get post by id
+// @access public
+router.get('/:id', (req, res) => {
+  Post.findById(req.params.id)
+    .then(post => res.json(post))
+    .catch(err => res.status(404));
+})
+
+// @route GET api/posts
 // @desc Create post
 // @access private
 router.post(
@@ -22,7 +41,7 @@ router.post(
 
     // Check validtion
     if (!isValid) {
-      // if any errors, send 400 with errors object
+      // if any errors, send error 400 with errors object
       return res.status(400).json(errors);
     }
 
